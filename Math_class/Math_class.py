33717843aebc2008ee670e88f4c8a992ce6c85e7
ryanpereira4099 <ryantonypereira@gmail.com>
2024-02-08 13:47:57 -0600

    Classes instated
    Math class and children classes are instated in the program

---
+++ Math_class/Math_class.py
@@ -0,0 +1,15 @@
+class math:
+    class Circle:
+        pass
+    class Triangle:
+        pass
+    class Rectangle:
+        pass
+    class Square:
+        pass
+    class Eclipse:
+        pass
+    class Trapezoid:
+        pass
+    class Parallelogram:
+        pass
