406f1b81f768d65339dd0e41390e467e4b919f85
ryanpereira4099 <145312422+Ry-Pereira@users.noreply.github.com>
2024-02-12 09:22:37 -0600

    File management and Creation of Classes
    Created, relocated, removed files. Creates new classes and python files for applications.

---
+++ Applications/Contact_Manager/Contact_manager.py
@@ -0,0 +1,21 @@
+print("Welcome to Contact Manager Program")
+
+user_account_inquiry = input("Do you have an account already (y/n) ?")
+
+
+
+
+
+
+def main():
+    print("Welcome to Contact Manager Program\n")
+    user_account_inquiry = input("Do you have an account already (y/n) ?")
+    if user_account_inquiry == "y":
+        account_login = 
+    else:
+        
+        pass
+
+
+
+main()
---
+++ Classes/account_login_class.py
@@ -0,0 +1,5 @@
+class Account_login:
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+        
