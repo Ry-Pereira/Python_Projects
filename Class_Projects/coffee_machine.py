35921eeab2c7e4ae5caffc0a6892dcdb9643b08b
ryanpereira4099 <145312422+Ry-Pereira@users.noreply.github.com>
2024-02-14 11:35:04 -0600

    Update coffee_machine.py

---
+++ Class_Projects/coffee_machine.py
@@ -0,0 +1,83 @@
+
+#resources----------------------------------------------------------------------
+
+machine_resources = {
+    "water" : 300,
+    "milk" : 200,
+    "coffee" : 100,
+    "money" : 0.0
+}
+
+menu_options = {
+    "espresso" : {
+        'water': 50,
+        "coffee": 18,
+        "price":1.50
+    },
+    "latte": {
+        'water': 200,
+        'coffee': 24,
+        'milk': 150,
+        "price":2.50
+    },
+    "cappuchino":{
+        'water': 250,
+        'coffee': 24,
+        'milk': 100,
+        "price":3.00
+    }
+
+    
+}
+
+currency = {
+    "penny": 0.01,
+    "nickel":0.05,
+    "dime": 0.10,
+    "quarter": 0.25,
+    "dollar":1.00
+}
+
+#methods--------------------------------------------------------------------------------
+
+def print_report(machine_resources= machine_resources)  :
+    for item in machine_resources:
+        print(item,"has about",machine_resources[item])
+
+def print_menu(menu = menu_options):
+    print("---MENU---\n")
+    for item in menu_options:
+        for attribute in (menu_options[item]):
+            if attribute == "price":
+                print(item,":",(menu_options[item][attribute]),"$\n")
+            
+      
+def check_resources(choice,menu_options = menu_options,machine_resources = machine_resources):
+    
+
+
+
+
+def choice(choice):
+    if choice == "espresso":
+        print("espresso")
+    elif choice == "latte":
+        print('latte')
+    elif choice == 'cappucino':
+        print('cappucino')
+    else:
+        print("Not a valid choice")
+        
+
+#main --------------------------------------------------------------------------------
+def main():
+    print("Welcome To The Coffee Machine\n")
+    print_menu()
+    choice(choice = input("What would you like to drink: "))
+
+
+
+
+
+
+main()
