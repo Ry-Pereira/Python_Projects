473cd082f2850f4b1bcde699ffd1ecb357ba5993
ryanpereira4099 <145312422+Ry-Pereira@users.noreply.github.com>
2024-03-08 11:37:44 -0600

    Update caesar_cypher.py

--- Translators/Unfinished/caesar_cipher/caesar_cypher.py
+++ Translators/Unfinished/caesar_cipher/caesar_cypher.py
@@ -4,21 +4,44 @@ alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
 
 
 
+
+      
+
 def intro_to_program():
     print("Welcome to Casear Cypher Program\n")
+    print(alphabet.index("a"))
 
 
 def print_menu():
     print("1. To Cypher Code")
     print("2. To Decypher Code\n")
 
+
+
+    
+
 def menu_selection(choice):
     if choice == 1:
-        cypher_code()
+        cypher_code(word_to_cypher = input("Word to Cypher: "))
     elif choice ==2:
-        decypher_code()
+        decypher_code(word_to_decypher = input("Word to Decypher: "))
     else:
         print("Goodbye")
+
+
+def cypher_code(word_to_cypher):
+    cyphered_word = []
+    amount_of_shifts = int(input("Aount of shift"))
+    for letter in word_to_cypher:
+        cyphered_word.append(alphabet[(alphabet.index(letter)) + amount_of_shift])
+        
+
+
+
+def decypher_code(word_to_decypher):
+    pass
+
+        
 def main():
     intro_to_program()
     print_menu()
