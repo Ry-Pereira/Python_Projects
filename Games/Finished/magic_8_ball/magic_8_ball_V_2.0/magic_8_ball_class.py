40034239ed2d859594a69508bc244158ee3b247a
ryanpereira4099 <145312422+Ry-Pereira@users.noreply.github.com>
2024-03-14 15:26:33 -0500

    Updated Version 2.0 of the Magic 8 Ball Program

---
+++ Games/Finished/magic_8_ball/magic_8_ball_V_2.0/magic_8_ball_class.py
@@ -0,0 +1,13 @@
+import random
+
+class Magic_8_Ball:
+    def __init__(self,response_pool):
+        self.response_pool = response_pool
+
+    def shake(self):
+        
+        return ("Magic 8 Ball:",random.choice(self.response_pool)
+
+    
+        
+
---
+++ Games/Finished/magic_8_ball/magic_8_ball_V_2.0/magic_8_ball_main.py
@@ -0,0 +1,11 @@
+from magic_8_ball_class import Magic_8_Ball
+from magic_8_ball_responses import responses
+
+
+
+magic_8_ball = Magic_8_Ball(responses)
+
+user_question = input("Question: ")
+
+
+
