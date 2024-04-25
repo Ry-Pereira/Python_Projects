ea18a48e3c3c643f06049d37e6002de343687bd3
ryanpereira4099 <145312422+Ry-Pereira@users.noreply.github.com>
2024-04-24 23:09:57 -0500

    Updated code for Rock Paper Scissors Game

---
+++ Games/Unfinished/rock_paper_scissors/rock_paper_scissor_object.py
@@ -0,0 +1,6 @@
+class RockPaperScissorObject:
+
+    def __init__(self,value,strength,weakness):
+        self.value = value
+        self.strength = strength
+        self.weakness = weakness
--- Games/Unfinished/rock_paper_scissors/rock_paper_scissors.py
+++ Games/Unfinished/rock_paper_scissors/rock_paper_scissors.py
@@ -1,83 +1,65 @@
-
 import random
+from rock_paper_scissor_object import RockPaperScissorObject
+class Game:
 
-game_moves = ["rock","paper","scissors"]
+    def __init__(self):
+        self.rock = RockPaperScissorObject("Rock","Scissors","Paper")
+        self.paper = RockPaperScissorObject("Paper","Rock","Scissors")
+        self.scissors = RockPaperScissorObject("Scissors","Paper","Rock")
+        self.bank = [self.rock,self.paper,self.scissors]
 
-print("Welcome To The Rock Paper Scissors Game Program")
-
-
-def print_menu():
-    print("Press 1: To Duel against the computer")
-    print("Press 2: To Duel agaisnt other player next you")
-
-def move_list():
-    print("Press 1 for Rock: Weakness: Paper ,Strength against: scissors\n ")
-    print("Press 2 for Paper: Weakness: Scissors ,Strength against: Rock\n")
-    print("Press 3 for Scissors: Weakness: Rock ,Strength against: Paper\n ")
-
-def mode_selection(mode):
-    if mode == 1:
-        print("Dueling against.... the computer")
-        mode_1()
-    if mode == 3:
-        print("Dueling against.... the other person")
-
+    def battle(self,other):
+        if self.stength == other.value:
+            return self.value
+        elif self.strength == other.weakness:
+            return other.value
+        else:
+            return "Tie"
         
+    def intro_to_program(self):
+        print("Welcome To The Rock Paper Scissors Game Program")
 
 
-def mode_1():
-    rounds = int(input("Best of how many rounds"))
-    round_counter = 0
-    player_win_count = 0
-    computer_win_count = 0
-    while round_counter != rounds:
-        move_list()
-        player_select = game_moves[int(input("Select move: ")) - 1]
-        computer_select = random.choice(game_moves)
-        print("Player:",player_select,"Computer:",computer_select)
-        if player_select == "scissors" and computer_select == "paper":
-            player_win_count +=1
-            print("Player won round",str(round_counter))
+    def print_menu(self):
+        print("Press 1: To Duel against the computer")
+        print("Press 2: To Exit Program")
+
+    def move_list(self):
+        print("Press 1 for Rock: Weakness: Paper ,Strength against: scissors\n ")
+        print("Press 2 for Paper: Weakness: Scissors ,Strength against: Rock\n")
+        print("Press 3 for Scissors: Weakness: Rock ,Strength against: Paper\n ")
+
+
+    def battling(self,choice):
+        rounds = 3
+        while rounds != 0:
+            choice = int(input("Choice: "))
+            if user_move == 1:
+                 if battle(self.rock,random.choice(self.bank)
+            if user_move == 2:
+                battle(self.paper,random.choice(self.bank)
+            if user_move == 3:
+                battle(self.scissor,random.choice(self.bank)
             
-        elif player_select == "scissors" and computer_select == "rock":
-            computer_win_count += 1
-            print("Computer won round",str(round_counter))
-        elif player_select == "paper" and computer_select == "rock":
-            player_win_count += 1
-            print("Player won round",str(round_counter))
-        elif player_select == "paper" and computer_select == "scissors":
-           computer_win_count += 1
-           print("Computer won round",str(round_counter))
-        elif player_select == "rock" and computer_select == "paper":
-            computer_win_count += 1
-            print("Computer won round",str(round_counter))
-        elif player_select == "rock" and computer_select == "scissors":
-            player_win_count += 1
-            print("Player won round",str(round_counter))
-        elif player_select == "scissors" and computer_select == "scissors":
-            print("tie")
+                
+                
             
-        elif player_select == "paper" and computer_select == "paper":
-            print("tie")
-            
-        elif player_select == "rock" and computer_select == "rock":
-            print("tie")
-        print(player_win_count)
-        print(computer_win_count)
-        round_counter = +1
-        print(round_counter)
         
+    def mode_selection(self,choice):
+        if choice == 1:
+            print("Dueling against.... the computer")
+            self.move_list()
         
-    if player_win_count > computer_win_count:
-        print("Player has won")
-    else:
-        print("Computer has won")
+        elif choice == 2:
+            print("Exiting Program...")
+        else:
+            print("Coming Soon")
+   
 
-        
-
-def main():
-    print_menu()
-    mode_selection(mode = int(input("What mofr will you select: ")))
+    def run(self):
+        self.intro_to_program
+        self.print_menu()
+        self.mode_selection(mode = int(input("CHoice: ")))
     
 
 
