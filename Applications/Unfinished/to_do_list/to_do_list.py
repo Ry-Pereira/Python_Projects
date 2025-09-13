fa713d99945de71b921d53097ed464c0591d66ab
Ryan <ryantonypereira@gmail.com>
2025-09-13 17:20:06 -0500

    Update to_do_list.py
    Created a menu and sub menu function to display to user what their options are to their todo lists. Created a run function to run the todo list program

--- Applications/Unfinished/to_do_list/to_do_list.py
+++ Applications/Unfinished/to_do_list/to_do_list.py
@@ -3,7 +3,21 @@ from linked_list import *
 
 class ToDoList:
     def __init__(self):
-        self.list = LinkedList()
+        self.to_do_lists = []
+
+    def main_menu(self):
+        print("1:Add todo list to todo lists")
+        print("2: Delete todo list from todo lists")
+        print("3: Edit todo list from todo lists")
+        print("4: Print todo list from todo lists")
+        print("5: Print todo list length from todo lists")
+
+    def sub_menu(self):
+        print("1:Add item to do list")
+        print("2:Remove item from to do list")
+        print("3:Search item from to do list")
+        print("4:Update item from to do list")
+
 
     def add_item(self,item):
         self.list.add(item)
@@ -16,3 +30,10 @@ class ToDoList:
 
     def delete_items(self):
         self.list.erase()
+
+
+    def run():
+        print("Welcome to the To DO List Program")
+        print("Made By Ryan Pereira\n\n")
+
+
