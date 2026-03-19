1896c55cb37cf521743d4916a3a277b0f0397dce
Ryan <ryantonypereira@gmail.com>
2026-03-18 20:25:54 -0500

    Added Code
    Added starter python code and documentation, and added a csv file to track all drafted emails.

---
+++ Tkinter_Projects/Quick_Email_Sender/quick_email_sender.py
@@ -0,0 +1,45 @@
+#Name: Ryan Pereira
+#Project Name: Quick Email Sender
+#Description:# A Quick Email Sender
+#Collaborators: None
+#Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
+#Date: 6/18/2026
+#Last Modified: 6/18/2026
+
+
+
+
+import smtplib
+from tkinter import *
+
+window = None
+
+
+
+
+
+def window_setup():
+    window = Tk()
+    window.title("Quick Email Sender")
+    return window
+
+
+
+def label_setup():
+    pass
+
+def button_setup():
+    pass
+
+
+
+
+def main():
+    window = window_setup()
+    window.mainloop()
+
+
+
+
+if __name__ == "__main__":
+    main()
