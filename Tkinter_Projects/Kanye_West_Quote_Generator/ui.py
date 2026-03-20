ceac4a69abbf8649517d0ee9ff78debe9d02ec0e
Ryan <ryantonypereira@gmail.com>
2026-03-20 15:46:07 -0500

    Image Selection
    Added more images for speech bubble option for text t0 be place in

--- Tkinter_Projects/Kanye_West_Quote_Generator/ui.py
+++ Tkinter_Projects/Kanye_West_Quote_Generator/ui.py
@@ -1,23 +1,29 @@
 from tkinter import *
 
-
+BACKGROUND_COLOR = "#8CC7C4"
+WINDOW_COLOR = "#2C687B"
+TITLE_COLOR = "white"
 class KanyeWestGeneratorUI:
     def __init__(self):
         self.window = Tk()
         self.window.title("Kanye West Quote Generator")
-        self.window.config(padx=20,pady=20)
+        self.window.config(padx=20,pady=20,bg=WINDOW_COLOR)
 
 
-        self.canvas = Canvas(width = 600,height =600)
-        self.canvas.grid(row=1,column=1)
+        self.title_label = Label(text="Kanye West Quote Generator",font=("Arial",24,"bold italic"),bg=WINDOW_COLOR,fg=TITLE_COLOR)
+        self.title_label.grid(row=0,column=0,columnspan=2)
+
+        self.canvas = Canvas(width = 600,height=600,bg=BACKGROUND_COLOR,highlightthickness=0)
+        self.canvas.grid(row=1,column=0,columnspan=2,pady=10)
 
 
         self.kanye_image = PhotoImage(file="kanye.png")
-        self.canvas.create_image(40,40,image = self.kanye_image)
+        self.canvas.create_image(150,500,image = self.kanye_image)
 
-        self.title_label = Label(text="Kanye West Quote Generator",font=("Arial",12))
-        self.title_label.grid(row=0,column=0,columnspan=3)
+        self.bubble = PhotoImage(file="speech_bubble.png")
+        self.canvas.create_image(150,500,image = self.bubble)
 
+        
 
         self.window.mainloop()
 
