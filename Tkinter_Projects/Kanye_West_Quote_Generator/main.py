9cef63dba87cb5fef2dc99f98bf2827544dc79cf
Ryan <ryantonypereira@gmail.com>
2026-03-21 16:11:40 -0500

    Added Documentation
    Added prologue comment to main program

--- Tkinter_Projects/Kanye_West_Quote_Generator/main.py
+++ Tkinter_Projects/Kanye_West_Quote_Generator/main.py
@@ -1,3 +1,14 @@
+#Name: Ryan Pereira
+#Project Name: Kanye West Quote Generator
+#Description:# A Tkinter-based program that fetches api data from a Kanye Quote source that distributes a random quote,quoted by Kanye West himself. Everytime the user clicks on the Kany West memoji icon, a new quote will appear in a random color speech bubble.
+#Module Name: Main
+#Collaborators: None
+#Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
+#Date: 6/19/2026
+#Last Modified: 6/21/2026
+
+
+#From the ui module,imports the KanyeWestGeneratorUI class to 
 from ui import KanyeWestGeneratorUI
 
 
