0cf2b54bbbbd1558067a38b4473c465d126f5d56
Ryan <ryantonypereira@gmail.com>
2026-03-21 16:22:24 -0500

    Added Documentation
    Added Documentation for the main program and prologue comments and other comments for the data file.

--- Tkinter_Projects/Kanye_West_Quote_Generator/kanye_west_quote_data.py
+++ Tkinter_Projects/Kanye_West_Quote_Generator/kanye_west_quote_data.py
@@ -1,9 +1,23 @@
+#Name: Ryan Pereira
+#Project Name: Kanye West Quote Generator
+#Description:# A Tkinter-based program that fetches api data from a Kanye Quote source that distributes a random quote,quoted by Kanye West himself. Everytime the user clicks on the Kany West memoji icon, a new quote will appear in a random color speech bubble.
+#Module Name: kanye_west_quote_data
+#Module Purpose: The kany_west_quote_data is resposible for the repsonse fetching and storing to be used in the UI
+#Collaborators: None
+#Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
+#Date: 6/19/2026
+#Last Modified: 6/21/2026
+
+#Importing the requests module, in order for API retrieval,storing, and accessing to be made possible.
 import requests
 
 
 
-
-def get_response():
+#The function get response will get a response from the API address that contains a random quote from Kanye West and return the response and quote
+def get_response() -> tuple:
+    #Reponse is set to thhe API repsonse of getting the API info from the url address
     response = requests.get(url="https://api.kanye.rest")
+    #Quote is seto the reponse, as the JSON file accessed, with the quote data being accessed
     quote = response.json()["quote"]
+    #Returns the response and quote
     return response,quote
--- Tkinter_Projects/Kanye_West_Quote_Generator/main.py
+++ Tkinter_Projects/Kanye_West_Quote_Generator/main.py
@@ -2,6 +2,7 @@
 #Project Name: Kanye West Quote Generator
 #Description:# A Tkinter-based program that fetches api data from a Kanye Quote source that distributes a random quote,quoted by Kanye West himself. Everytime the user clicks on the Kany West memoji icon, a new quote will appear in a random color speech bubble.
 #Module Name: Main
+#Module Purpose: This porgram file is the entyr point into the project program itself.
 #Collaborators: None
 #Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
 #Date: 6/19/2026
@@ -12,8 +13,13 @@
 from ui import KanyeWestGeneratorUI
 
 
+#The function main, is the main entry point into the program
 def main():
+    #The kanywestui variable stores the object of the KanywestGeneratorUI class constructor
     kanywestui = KanyeWestGeneratorUI()
     
+
+#If the progrma is being run directly, then the main function will be called
 if __name__=="__main__":
+    #The main function will be called
     main()
