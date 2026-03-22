bbd9bb221efb2ceaf8d5b9c8e5902c96f68b3f2e
Ryan <ryantonypereira@gmail.com>
2026-03-22 14:22:19 -0500

    Updated UI
    Added color to the UI for greater appeal.

--- Tkinter_Projects/Quick_Email_Sender/ui.py
+++ Tkinter_Projects/Quick_Email_Sender/ui.py
@@ -21,52 +21,61 @@ class UI:
         self.entries = []
         self.window = Tk()
         self.window.title("Quick Email Sender")
-        self.window.config(padx=20,pady=20)
+        self.window.config(padx=40,pady=40,bg="#F4EBD3")
 
-        self.title_label = Label(text="Quick Email Sender",font=("Arial",24))
+        self.title_label = Label(text="Quick Email Sender",font=("Arial",24),bg="#F4EBD3",fg="#DED3C4")
         self.title_label.grid(row=0,column=0,columnspan=4,pady=10)
 
-        self.to_section_label = Label(text="To:",font=("Arial",20))
-        self.to_section_label.grid(row=1,column=0)
+        self.from_section_label = Label(text="From:",font=("Arial",20),bg="#F4EBD3",fg="#DED3C4")
+        self.from_section_label.grid(row=1,column=0)
+
+
+        self.to_section_label = Label(text="To:",font=("Arial",20),bg="#F4EBD3",fg="#DED3C4")
+        self.to_section_label.grid(row=2,column=0)
     
 
 
-        self.subject_section_label = Label(text="Subject:",font=("Arial",20,))
-        self.subject_section_label.grid(row=2,column=0)
+        self.subject_section_label = Label(text="Subject:",font=("Arial",20,),bg="#F4EBD3",fg="#DED3C4")
+        self.subject_section_label.grid(row=3,column=0)
 
-        self.message_section_label = Label(text="Message:",font=("Arial",20))
-        self.message_section_label.grid(row=3,column=0)
+        self.message_section_label = Label(text="Message:",font=("Arial",20),bg="#F4EBD3",fg="#DED3C4")
+        self.message_section_label.grid(row=4,column=0)
 
+        self.from_entry = Text(width=20,height=2,bg="#98A1BC")
+        self.from_entry.grid(row=1, column=1, columnspan=3, sticky="ew",pady=(10, 0))
 
-        self.to_entry = Text(width=20,height=2)
-        self.to_entry.grid(row=1, column=1, columnspan=3, sticky="ew",pady=(10, 0))
+        self.password_entry = Text(width=20,height=2,bg="#98A1BC")
+        self.password_entry.grid(row=2, column=1, columnspan=3, sticky="ew",pady=(10, 0))
+
+        self.to_entry = Text(width=20,height=2,bg="#98A1BC")
+        self.to_entry.grid(row=3, column=1, columnspan=3, sticky="ew",pady=(10, 0))
     
 
-        self.subject_entry = Text(width=20,height=2)
-        self.subject_entry.grid(row=2, column=1, columnspan=3, sticky="ew",pady=(10, 0))
+        self.subject_entry = Text(width=20,height=2,bg="#98A1BC")
+        self.subject_entry.grid(row=4, column=1, columnspan=3, sticky="ew",pady=(10, 0))
 
-        self.message_entry = Text(width=40,height=12)
-        self.message_entry.grid(row=4,column=0,columnspan=4,sticky="ew",pady=(20, 0))
+        self.message_entry = Text(width=40,height=12,bg="#98A1BC")
+        self.message_entry.grid(row=6,column=0,columnspan=4,sticky="ew",pady=(20, 0))
 
         #self.entries.append(self.to_entry)
         #self.entries.append(self.subject_entry)
 
 
-        self.send_button = Button(text="Send",command=self.send_email,font=('Arial',12))
-        self.send_button.grid(row=5,column=0,sticky="ew",pady=(20, 0))
+        self.send_button = Button(text="Send",command=self.send_email,font=('Arial',12),bg="#555879",fg="#DED3C4")
+        self.send_button.grid(row=7,column=0,sticky="ew",pady=(20, 0))
   
 
 
-        self.clear_button = Button(text="Clear",command = self.clear_all_sections,font=('Arial',12))
-        self.clear_button.grid(row=5,column=1,sticky="ew",pady=(20, 0))
+        self.clear_button = Button(text="Clear",command = self.clear_all_sections,font=('Arial',12),bg="#555879",fg="#DED3C4")
+        self.clear_button.grid(row=7,column=1,sticky="ew",pady=(20, 0))
        
 
-        self.save_button = Button(text="Save",command=self.save_draft,font=('Arial',12))
-        self.save_button.grid(row=5,column=2,sticky="ew",pady=(20, 0))
+        self.save_button = Button(text="Save",command=self.save_draft,font=('Arial',12),bg="#555879",fg="#DED3C4")
+        self.save_button.grid(row=7,column=2,sticky="ew",pady=(20, 0))
    
 
-        self.exit_button = Button(text="Exit",command = self.exit_program,font=('Arial',12))
-        self.exit_button.grid(row=5,column=3,sticky="ew",pady=(20, 0))
+        self.exit_button = Button(text="Exit",command = self.exit_program,font=('Arial',12),bg="#555879",fg="#DED3C4")
+        self.exit_button.grid(row=7,column=3,sticky="ew",pady=(20, 0))
 
         for i in range(4):
             self.window.grid_columnconfigure(i, weight=1,uniform="a")
