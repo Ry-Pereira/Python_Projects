f88b1de95c3e2425857b5ee97cc90d017160906a
Ryan <ryantonypereira@gmail.com>
2026-03-22 13:35:00 -0500

    UI Upgrade
    UI improvement in sizing, spacing, and general appeal.

--- Tkinter_Projects/Quick_Email_Sender/ui.py
+++ Tkinter_Projects/Quick_Email_Sender/ui.py
@@ -26,50 +26,50 @@ class UI:
         self.title_label = Label(text="Quick Email Sender",font=("Arial",24))
         self.title_label.grid(row=0,column=0,columnspan=4,pady=10)
 
-        self.to_section_label = Label(text="To:",font=("Arial",18),width=2,bg="yellow")
+        self.to_section_label = Label(text="To:",font=("Arial",20))
         self.to_section_label.grid(row=1,column=0)
     
 
 
-        self.subject_section_label = Label(text="Subject:",font=("Arial",20,),width=10)
+        self.subject_section_label = Label(text="Subject:",font=("Arial",20,))
         self.subject_section_label.grid(row=2,column=0)
 
         self.message_section_label = Label(text="Message:",font=("Arial",20))
         self.message_section_label.grid(row=3,column=0)
 
 
-        self.to_entry = Text(width=20,height=1)
-        self.to_entry.grid(row=1,column=1)
+        self.to_entry = Text(width=20,height=2)
+        self.to_entry.grid(row=1, column=1, columnspan=3, sticky="ew",pady=(10, 0))
     
 
-        self.subject_entry = Text(width=20,height=1)
-        self.subject_entry.grid(row=2,column=1)
+        self.subject_entry = Text(width=20,height=2)
+        self.subject_entry.grid(row=2, column=1, columnspan=3, sticky="ew",pady=(10, 0))
 
-        self.message_entry = Text(width=40,height=10)
-        self.message_entry.grid(row=4,column=0,columnspan=4,sticky="ew")
+        self.message_entry = Text(width=40,height=12)
+        self.message_entry.grid(row=4,column=0,columnspan=4,sticky="ew",pady=(20, 0))
 
         #self.entries.append(self.to_entry)
         #self.entries.append(self.subject_entry)
 
 
         self.send_button = Button(text="Send",command=self.send_email,font=('Arial',12))
-        self.send_button.grid(row=5,column=0,sticky="ew")
+        self.send_button.grid(row=5,column=0,sticky="ew",pady=(20, 0))
   
 
 
         self.clear_button = Button(text="Clear",command = self.clear_all_sections,font=('Arial',12))
-        self.clear_button.grid(row=5,column=1,sticky="ew")
+        self.clear_button.grid(row=5,column=1,sticky="ew",pady=(20, 0))
        
 
         self.save_button = Button(text="Save",command=self.save_draft,font=('Arial',12))
-        self.save_button.grid(row=5,column=2,sticky="ew")
+        self.save_button.grid(row=5,column=2,sticky="ew",pady=(20, 0))
    
 
         self.exit_button = Button(text="Exit",command = self.exit_program,font=('Arial',12))
-        self.exit_button.grid(row=5,column=3,sticky="ew")
+        self.exit_button.grid(row=5,column=3,sticky="ew",pady=(20, 0))
 
         for i in range(4):
-            self.window.grid_columnconfigure(i, weight=1)
+            self.window.grid_columnconfigure(i, weight=1,uniform="a")
 
         self.window.mainloop()
 
