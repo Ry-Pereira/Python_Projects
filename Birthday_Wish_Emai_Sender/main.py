6470b49eb12896d46942a063c9784c5c20f92fcb
Ryan <ryantonypereira@gmail.com>
2026-03-25 11:42:45 -0500

    Update main.py
    Added Code to update csv file with new birthday information.

--- Birthday_Wish_Emai_Sender/main.py
+++ Birthday_Wish_Emai_Sender/main.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import pandas
 import smtplib
 
 EMAIL = ""
@@ -7,16 +8,32 @@ TO_ADDRESS = ""
 
 date_of_birth = dt.datetime(year=2004,month=2,day=17,hour=12)
 
-def get_time():
-    now_time = dt.datetime.now()
-    now_month = now_time.month
-    now_day = now_time.day
-    now_hour = now_time.hour
-    print(now_time.month)
-    print(now_time.day)
-    print(now_time.hour-12)
+def store_birthday_info():
+    birthday_year = int(input("Year of Birth: "))
+    birthday_month = int(input("Year of Month: "))
+    birthday_day = int(input("Year of Day: "))
+    birthday_hour = int(input("Hour to send Birthday"))
+    post_or_past_meridian  = input("Past or Post Meridian:")
+    birthday_first_name = input("Birthday First Name: ")
+    birthday_last_name = input("Birthday Last Name: ")
+    birthday_email = input("Birthday Email To Send: ")
 
-    print(now_time)
+    if post_or_past_meridian == "PM":
+        birthday_hour +=12
+
+
+    new_birthday_info = pandas.DataFrame({
+        "First_Name": birthday_first_name,
+        "Last_Name": birthday_last_name,
+        "Email": birthday_email,
+        "Year": birthday_year,
+        "Month":birthday_month,
+        "Day":birthday_day,
+        "Time":birthday_hour
+    })
+
+    new_birthday_info.to_csv("birthday.csv",mode="a",header=False,index=False)
+   
 
 def send_email():
     with smtplib.SMTP("smtp.gmail.com") as connection:
