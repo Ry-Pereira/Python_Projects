88a64dfe5fc4e863bf2432b2cb9b5c7d93a08446
Ryan <ryantonypereira@gmail.com>
2026-03-30 13:35:32 -0500

    Documentation
    Added prologue comment. Added other comments for parts of code.

--- Habit_Tracker/main.py
+++ Habit_Tracker/main.py
@@ -1,14 +1,27 @@
+#Name: Ryan Pereira
+#Project Name: Graph Pixel Tracker
+#Module Name: Main
+#Module Purpose: The moudle is the main entry into the program. It will ask the user for all the necessary information to create a graph and update it. It will also allow the user to edit and delete pixels from the graph.
+#Description: A Graph Pixel Tracker is a tool that allows users to track their habits and activities by creating a graph of pixels. Each pixel represents a day and the color of the pixel represents the quantity of the activity. The user can update the graph by adding new pixels, editing existing pixels, and deleting pixels. The graph can be used to visualize the user's progress and motivate them to continue their habits.
+#Collaborators: None
+#Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
+#Date: 6/30/2026
+#Last Modified: 6/30/2026
+
+#This program uses the Pixela API to create a graph and track habits. The user will be prompted to enter their username, token, and other necessary information to create a graph. The user can then update the graph by adding new pixels, editing existing pixels, and deleting pixels. The program will use the requests library to make API calls to the Pixela API.
 import requests
+#The datetime module is used to get the current date in the format of YYYYMMDD, which is required by the Pixela API when creating and updating pixels.
 import datetime as dt
 
-
+#The user is prompted to enter their username, token, and whether they are a minor. This information is necessary to create a user account on the Pixela platform and to authenticate API requests. The token is a unique identifier that allows the user to access their account and manage their graphs and pixels.
 DATE_TODAY = dt.datetime.now().strftime("%Y%m%d")
 USERNAME = input("Enter you username: ")
 TOKEN = input("Enter your own custom token: ")
 IS_MINOR = input("Are you minor? (yes/no): ")
 
-
+#Pixela API endpoint for creating a user. The user will be created with the provided username and token. The user must agree to the terms of service and confirm that they are not a minor to create an account.
 pixela_endpoint = "https://pixe.la/v1/users"
+#The user parameters are stored in a dictionary that will be sent as a JSON payload in the API request to create a user. The parameters include the token, username, agreement to terms of service, and confirmation of not being a minor.
 user_parameters = {
     "token": TOKEN,
     "username": USERNAME,
