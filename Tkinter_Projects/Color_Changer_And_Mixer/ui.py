4f2caf18009dae23a24fe0e7d131ebb6220c0f25
Ryan <ryantonypereira@gmail.com>
2026-04-08 19:50:55 -0500

    Added a module
    Added and imported that module with color information for better modularity.

--- Tkinter_Projects/Color_Changer_And_Mixer/ui.py
+++ Tkinter_Projects/Color_Changer_And_Mixer/ui.py
@@ -1,23 +1,6 @@
 from tkinter import *
 from functools import partial
-
-
-#Colors with associated rgb value tuples to represent them
-RED = (214, 33, 33)
-ORANGE = (214, 103, 33)
-YELLOW = (214, 211, 33)
-LIME = (151, 214, 33)
-GREEN = (63, 214, 33)
-TEAL = (33, 214, 178)
-LIGHT_BLUE = (33, 208, 214)
-INDIGO = (33, 118, 214)
-BLUE = (33, 54, 214)
-PURPLE = (118, 33, 214)
-VIOLET = (175, 33, 214)
-PINK = (214, 33, 193)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GRAY = (107, 107, 107)
+from colors import *
 
 
 
