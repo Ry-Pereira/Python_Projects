9e6594ce62e9c6a1b23451d8dcf8cdda2706dcca
Ryan <ryantonypereira@gmail.com>
2026-04-08 19:36:29 -0500

    Writing Code
    Added Code

--- Tkinter_Projects/Color_Changer_And_Mixer/ui.py
+++ Tkinter_Projects/Color_Changer_And_Mixer/ui.py
@@ -1,6 +1,5 @@
 from tkinter import *
-
-
+from functools import partial
 
 
 #Colors with associated rgb value tuples to represent them
@@ -26,64 +25,69 @@ class ColorChangerAndMixerUI:
     def __init__(self):
         self.window = Tk()
         self.window.title("Color Changer And Mixer")
+        self.canvas_background_color = #FFFFFF
         self.window.config(padx=50,pady=50,bg="white")
 
-        self.title_label = Label(text="COLOR CHANGER AND MIXER", font=("Arial",24),bg="white")
+        self.color_mix = []
+
+        self.title_label = Label(text="COLOR CHANGER AND MIXER", font=("Arial",18),bg="white")
         self.title_label.grid(row=0,column=0,columnspan=5,pady=10)
 
-        self.title_label = Label(text="By: Ryan Pereira", font=("Arial",20),bg="white")
+        self.title_label = Label(text="By: Ryan Pereira", font=("Arial",16),bg="white")
         self.title_label.grid(row=1,column=0,columnspan=5,pady=10)
 
+        self.canvas = Canvas(width=850,height=300,bg=self.canvas_background_color,highlightthickness=0,bd=3,relief="solid")
+        self.canvas.grid(row=2,column=0,columnspan=5,padx=15,pady=15)
+                            
+        self.red_button = Button(text="RED",width=17,pady=15,padx=15,bg="red",command=partial(self.add_color_to_background_color,RED))
+        self.red_button.grid(row=3,column=0,pady=15,padx=15)
 
-        self.red_button = Button(text="RED",width=25,pady=15,padx=15,bg="red")
-        self.red_button.grid(row=2,column=0,pady=15,padx=15)
+        self.orange_button = Button(text="ORANGE",width=17,pady=15,padx=15,bg="orange",command=partial(self.add_color_to_background_color,ORANGE))
+        self.orange_button.grid(row=3,column=1,pady=15,padx=15)
 
-        self.orange_button = Button(text="ORANGE",width=25,pady=15,padx=15,bg="orange")
-        self.orange_button.grid(row=2,column=1,pady=15,padx=15)
+        self.yellow_button = Button(text="YELLOW",width=17,pady=15,padx=15,bg="yellow",command=partial(self.add_color_to_background_color,YELLOW))
+        self.yellow_button.grid(row=3,column=2,pady=15,padx=15)
 
-        self.yellow_button = Button(text="YELLOW",width=25,pady=15,padx=15,bg="yellow")
-        self.yellow_button.grid(row=2,column=2,pady=15,padx=15)
+        self.lime_button = Button(text="LIME",width=17,pady=15,padx=15,bg="lime",command=partial(self.add_color_to_background_color,LIME))
+        self.lime_button.grid(row=3,column=3,pady=15,padx=15)
 
-        self.lime_button = Button(text="LIME",width=25,pady=15,padx=15,bg="lime")
-        self.lime_button.grid(row=2,column=3,pady=15,padx=15)
-
-        self.green_button = Button(text="GREEN",width=25,pady=15,padx=15,bg="green")
-        self.green_button.grid(row=2,column=4,pady=15,padx=15)
-
-
-        self.teal_button = Button(text="TEAL",width=25,pady=15,padx=15,bg="teal")
-        self.teal_button.grid(row=3,column=0,pady=15,padx=15)
+        self.green_button = Button(text="GREEN",width=17,pady=15,padx=15,bg="green",command=partial(self.add_color_to_background_color,GREEN))
+        self.green_button.grid(row=3,column=4,pady=15,padx=15)
 
 
-        self.light_blue_button = Button(text="LIGHT-BLUE",width=25,pady=15,padx=15,bg="light blue")
-        self.light_blue_button.grid(row=3,column=1,pady=15,padx=15)
+        self.teal_button = Button(text="TEAL",width=17,pady=15,padx=15,bg="teal",command=partial(self.add_color_to_background_color,TEAL))
+        self.teal_button.grid(row=4,column=0,pady=15,padx=15)
 
 
-        self.indigo_button = Button(text="INDIGO",width=25,pady=15,padx=15,bg="indigo")
-        self.indigo_button.grid(row=3,column=2,pady=15,padx=15)
+        self.light_blue_button = Button(text="LIGHT-BLUE",width=17,pady=15,padx=15,bg="light blue",command=partial(self.add_color_to_background_color,LIGHT_BLUE))
+        self.light_blue_button.grid(row=4,column=1,pady=15,padx=15)
 
 
-        self.blue_button = Button(text="BLUE",width=25,pady=15,padx=15,bg="blue")
-        self.blue_button.grid(row=3,column=3,pady=15,padx=15)
-
-        self.purple_button = Button(text="PURPLE",width=25,pady=15,padx=15,bg="purple")
-        self.purple_button.grid(row=3,column=4,pady=15,padx=15)
-
-        self.violet_button = Button(text="VIOLET",width=25,pady=15,padx=15,bg="violet")
-        self.violet_button.grid(row=4,column=0,pady=15,padx=15)
+        self.indigo_button = Button(text="INDIGO",width=17,pady=15,padx=15,bg="indigo",command=partial(self.add_color_to_background_color,INDIGO))
+        self.indigo_button.grid(row=4,column=2,pady=15,padx=15)
 
 
-        self.pink_button = Button(text="PINK",width=25,pady=15,padx=15,bg="pink")
-        self.pink_button.grid(row=4,column=1,pady=15,padx=15)
+        self.blue_button = Button(text="BLUE",width=17,pady=15,padx=15,bg="blue",command=partial(self.add_color_to_background_color,BLUE))
+        self.blue_button.grid(row=4,column=3,pady=15,padx=15)
 
-        self.white_button = Button(text="WHITE",width=25,pady=15,padx=15,bg="white")
-        self.white_button.grid(row=4,column=2,pady=15,padx=15)
+        self.purple_button = Button(text="PURPLE",width=17,pady=15,padx=15,bg="purple",command=partial(self.add_color_to_background_color,PURPLE))
+        self.purple_button.grid(row=4,column=4,pady=15,padx=15)
 
-        self.black_button = Button(text="BLACK",width=25,pady=15,padx=15,bg="black")
-        self.black_button.grid(row=4,column=3,pady=15,padx=15)
+        self.violet_button = Button(text="VIOLET",width=17,pady=15,padx=15,bg="violet",command=partial(self.add_color_to_background_color,VIOLET))
+        self.violet_button.grid(row=5,column=0,pady=15,padx=15)
 
-        self.gray_button = Button(text="GRAY",width=25,pady=15,padx=15 ,bg="gray")
-        self.gray_button.grid(row=4,column=4,pady=15,padx=15)
+
+        self.pink_button = Button(text="PINK",width=17,pady=15,padx=15,bg="pink",command=partial(self.add_color_to_background_color,PINK))
+        self.pink_button.grid(row=5,column=1,pady=15,padx=15)
+
+        self.white_button = Button(text="WHITE",width=17,pady=15,padx=15,bg="white",command=partial(self.add_color_to_background_color,WHITE))
+        self.white_button.grid(row=5,column=2,pady=15,padx=15)
+
+        self.black_button = Button(text="BLACK",width=17,pady=15,padx=15,bg="black",command=partial(self.add_color_to_background_color,BLACK))
+        self.black_button.grid(row=5,column=3,pady=15,padx=15)
+
+        self.gray_button = Button(text="GRAY",width=17,pady=15,padx=15 ,bg="gray",command=partial(self.add_color_to_background_color,GRAY))
+        self.gray_button.grid(row=5,column=4,pady=15,padx=15)
 
         
 
@@ -91,11 +95,21 @@ class ColorChangerAndMixerUI:
         
         self.window.mainloop()
 
-    def add_color_to_background_color(self,color):
+    def add_color_to_background_color(self,rgb_color):
+        self.color_mix.append(rgb_color)
+        r_value = 0
+        g_value = 0
+        b_value = 0
+        for color in self.color_mix:
+            r_value += color[0]
+            g_value += color[1]
+            b_value += color[2]
+        self.canvas_background_color = (r_value//len(self.color_mix),g_value//len(self.color_mix),b_value//len(self.color_mix))
+        self.canvas.config(bg=self.convert_rgb_to_hex_color(self.canvas_background_color))
+
+    def remove_color_from_background_color(self,rgb_color):
         pass
 
-    def remove_color_from_background_color(self,color):
-        pass
 
     def convert_rgb_to_hex_color(rgb_color_value: tuple) -> str:
         return f"#{rgb_color_value[0]:02X}{rgb_color_value[1]:02X}{rgb_color_value[2]:02X}"
