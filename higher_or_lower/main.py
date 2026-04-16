3194eb1e51db0d3951f5606877de27f0d927049a
Ryan <ryantonypereira@gmail.com>
2026-04-16 17:03:13 -0500

    Starting files for new project
    Starting files for higher or lower game, with main and server python files

---
+++ higher_or_lower/main.py
@@ -0,0 +1,27 @@
+
+
+import random
+import server
+
+
+
+
+
+
+
+
+def main():
+    number = random.randint(0,9)
+    server.run(number)
+    
+    
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
